a7cbc75e5865f023eaeaef7406712793ac13ac73
tbecerro <thomas.becerro@widergy.com>
2020-07-30 16:06:37 -0300

    fix: delete default app

--- src/index.js
+++ src/index.js
@@ -5,13 +5,11 @@ import 'url-search-params-polyfill';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
 import { StylesProvider } from '@material-ui/styles';
 
 import './scss/index.scss';
 import './config/analytics';
 import './config/i18n';
-import store from './redux/store';
 import { unregister } from './serviceWorker';
 import App from './app';
 
@@ -22,9 +20,7 @@ unregister();
 
 ReactDOM.render(
   <StylesProvider injectFirst>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </StylesProvider>,
   document.getElementById('root')
 );
