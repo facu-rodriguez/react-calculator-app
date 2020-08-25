9ffb2d1d062f4128df89e970b06a5ad560ba4f89
facu-rodriguez <facu-rodriguez@widergy.com>
2020-08-25 14:24:07 -0300

    le agregue estilo al formulario

--- src/app/components/Form/index.js
+++ src/app/components/Form/index.js
@@ -2,6 +2,8 @@ import React, { PureComponent } from 'react';
 import { Field, reduxForm } from 'redux-form';
 import { Link } from 'react-router-dom';
 
+import styles from './style.module.scss';
+
 const text = (value, previousValue) => {
   if (value.slice(-1).match(/[^\d]/g)) {
     return value;
@@ -28,24 +30,48 @@ const phoneNumber = value => {
 class ReviewForm extends PureComponent {
   render() {
     return (
-      <form onSubmit={this.props.handleSubmit}>
-        <div>
-          <label>Name</label>
-          <Field name="firstName" component="input" type="text" normalize={text} />
+      <form className={styles.form} onSubmit={this.props.handleSubmit}>
+        <h1 className={styles.tittle}>Leave your comments here!</h1>
+        <div className={styles.formContainer}>
+          <div className={styles.fieldContainer}>
+            <div>
+              <label className={styles.label}>Name</label>
+              <br></br>
+              <Field
+                className={styles.fields}
+                name="firstName"
+                component="input"
+                type="text"
+                normalize={text}
+              />
+            </div>
+            <div>
+              <label className={styles.label}>Review</label>
+              <br></br>
+              <Field className={styles.fields} name="review" component="input" type="text" />
+            </div>
+            <div>
+              <label className={styles.label}>Phone Number</label>
+              <br></br>
+              <Field
+                className={styles.fields}
+                name="phoneNumber"
+                component="input"
+                type="text"
+                normalize={phoneNumber}
+              />
+            </div>
+            {this.props.error && <strong>{this.props.error}</strong>}
+          </div>
+          <div className={styles.buttonsContainer}>
+            <button className={styles.buttonCancel} type="button" onClick={this.props.onClick}>
+              <Link to="/">Cancel</Link>
+            </button>
+            <button className={styles.buttonSubmit} type="submit">
+              Submit
+            </button>
+          </div>
         </div>
-        <div>
-          <label>Review</label>
-          <Field name="review" component="input" type="text" />
-        </div>
-        <div>
-          <label>Phone Number</label>
-          <Field name="phoneNumber" component="input" type="text" normalize={phoneNumber} />
-        </div>
-        {this.props.error && <strong>{this.props.error}</strong>}
-        <button type="button" onClick={this.props.onClick}>
-          <Link to="/">Cancel</Link>
-        </button>
-        <button type="submit">Submit</button>
       </form>
     );
   }
